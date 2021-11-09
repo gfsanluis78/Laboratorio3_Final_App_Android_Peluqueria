42aa43fe9c94c62ebb68912c549dbf7c9155dd5c
gfsanluis78 <71910385+gfsanluis78@users.noreply.github.com>
2021-11-09 13:01:09 -0300

    Initial commit

---
+++ app/src/main/java/com/farias/laboratorio3_final_app_android_peluqueria/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.farias.laboratorio3_final_app_android_peluqueria.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/farias/laboratorio3_final_app_android_peluqueria/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.farias.laboratorio3_final_app_android_peluqueria.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
